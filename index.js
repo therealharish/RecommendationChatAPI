5adac2bcba35d2a3008f1fc4ce3c078601b1fd30
Harish <48938639+therealharish@users.noreply.github.com>
2023-10-19 13:43:04 +0530

    fixed ports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
-const PORT = 3000
+const PORT = process.env.PORT || 3001
 const findItems = require('./query.js')
 
 app.get('/', (request, response) => {
